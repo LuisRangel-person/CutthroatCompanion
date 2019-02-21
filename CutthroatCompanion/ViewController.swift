ba9c4a4fedf18e337baa883e90a601dc6d96ee5a
Luis Rangel <luisrangel.person@gmail.com>
2019-02-20 20:43:12 -0600

    Initial Commit

---
+++ CutthroatCompanion/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CutthroatCompanion
+//
+//  Created by Luis Rangel on 2/20/19.
+//  Copyright © 2019 Luis Rangel. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
